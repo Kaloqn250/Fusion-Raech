318bc4c5e1e763ebd284f5f652eb28e6e788d956
Kaloqn250 <kalojancoo39@gmail.com>
2026-01-13 02:43:33 +0200

    Created the jobs, freelancers and companies apps

--- Fusion_Reach/settings.py
+++ Fusion_Reach/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'common_app',
+    'jobs',
+    'freelancers',
+    'companies'
 ]
 
 MIDDLEWARE = [
@@ -74,12 +78,15 @@ WSGI_APPLICATION = 'Fusion_Reach.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "fusion_reach",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -116,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
